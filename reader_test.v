d344be5e2e9a16f246df62d6d6452bbf142ed1ff
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2022-08-27 12:03:20 +0900

    update v version (#2)

--- reader_test.v
+++ reader_test.v
@@ -2,12 +2,12 @@ module streader
 
 fn test_read() ? {
 	mut reader := new('abcde')
-	mut bw := []byte{len: 3}
+	mut bw := []u8{len: 3}
 
-	n0 := reader.read(mut bw) ?
+	n0 := reader.read(mut bw)?
 	assert n0 == 3
 
-	n1 := reader.read(mut bw) ?
+	n1 := reader.read(mut bw)?
 	assert n1 == 2
 
 	if _ := reader.read(mut bw) {
@@ -19,23 +19,23 @@ fn test_read() ? {
 
 fn test_len() ? {
 	mut reader := new('abcde')
-	mut bw := []byte{len: 3}
+	mut bw := []u8{len: 3}
 
 	assert reader.len() == 5
 
-	_ := reader.read(mut bw) ?
+	_ := reader.read(mut bw)?
 	assert reader.len() == 2
 
-	_ := reader.read(mut bw) ?
+	_ := reader.read(mut bw)?
 	assert reader.len() == 0
 }
 
 fn test_size() ? {
 	mut reader := new('abcde')
-	mut bw := []byte{len: 3}
+	mut bw := []u8{len: 3}
 
 	assert reader.size() == 5
 
-	_ := reader.read(mut bw) ?
+	_ := reader.read(mut bw)?
 	assert reader.size() == 5
 }
