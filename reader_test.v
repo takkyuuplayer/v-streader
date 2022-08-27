1b54540ef9cf7b98e2114799170e6ab9660a4a8f
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2022-08-27 13:10:15 +0900

    fix for v 0.3 (#3)

--- reader_test.v
+++ reader_test.v
@@ -1,41 +1,50 @@
 module streader
 
-fn test_read() ? {
+import io
+
+fn test_read() ! {
 	mut reader := new('abcde')
 	mut bw := []u8{len: 3}
 
-	n0 := reader.read(mut bw)?
+	n0 := reader.read(mut bw)!
 	assert n0 == 3
 
-	n1 := reader.read(mut bw)?
+	n1 := reader.read(mut bw)!
 	assert n1 == 2
 
 	if _ := reader.read(mut bw) {
 		assert false
 	} else {
-		assert err is none
+		assert err is io.Eof
 	}
 }
 
-fn test_len() ? {
+fn test_len() ! {
 	mut reader := new('abcde')
 	mut bw := []u8{len: 3}
 
 	assert reader.len() == 5
 
-	_ := reader.read(mut bw)?
+	_ := reader.read(mut bw)!
 	assert reader.len() == 2
 
-	_ := reader.read(mut bw)?
+	_ := reader.read(mut bw)!
 	assert reader.len() == 0
 }
 
-fn test_size() ? {
+fn test_size() ! {
 	mut reader := new('abcde')
 	mut bw := []u8{len: 3}
 
 	assert reader.size() == 5
 
-	_ := reader.read(mut bw)?
+	_ := reader.read(mut bw)!
 	assert reader.size() == 5
+}
+
+fn test_interface() {
+	iface := fn (_ io.Reader) bool {
+		return true
+	}
+	assert iface(new('abcde'))
 }
